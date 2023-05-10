330be433c7fc0e9aae987177fb9ee833f27e9198
Syed Luqman <32990257+syedluqman@users.noreply.github.com>
2023-05-10 22:37:29 +0100

    Add files via upload
    The first commit

---
+++ ingest.py
@@ -0,0 +1,22 @@
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.embeddings import LlamaCppEmbeddings
+from sys import argv
+
+def main():
+    # Load document and split in chunks
+    loader = TextLoader(argv[1], encoding="utf8")
+    documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+    texts = text_splitter.split_documents(documents)
+    # Create embeddings
+    llama = LlamaCppEmbeddings(model_path="./models/ggml-model-q4_0.bin")
+    # Create and store locally vectorstore
+    persist_directory = 'db'
+    db = Chroma.from_documents(texts, llama, persist_directory=persist_directory)
+    db.persist()
+    db = None
+
+if __name__ == "__main__":
+    main()
